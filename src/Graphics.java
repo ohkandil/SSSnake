b801635aa27e75c0c4dc6de7c9746dc91d1f6228
ohkandil <o.hossam@nu.edu.eg>
2023-03-26 15:17:48 +0200

    Version 1.11 Collision test successful

--- src/Graphics.java
+++ src/Graphics.java
@@ -55,6 +55,8 @@ public class Graphics extends JPanel implements ActionListener {
                             }
                             break;
                     }
+                } else {
+                    start();
                 }
             }
         });
@@ -63,42 +65,6 @@ public class Graphics extends JPanel implements ActionListener {
 
 
     }
-
-    @Override
-    protected void paintComponent(java.awt.Graphics g) {
-        super.paintComponent(g);
-        if (isMoving) {
-            g.setColor(Color.darkGray);
-            for (int i = 0; i < snakeLength; i++) {
-                g.fillRect(snakePosX[i], snakePosY[i], TICKSIZE, TICKSIZE);
-
-            }
-        }
-    }
-
-protected void move() {
-    for (int i = snakeLength; i > 0; i--) {
-        snakePosX[i] = snakePosX[i - 1];
-        snakePosY[i] = snakePosY[i - 1];
-    }
-    switch (direction) {
-        case 'U':
-            snakePosY[0] = snakePosY[0] - TICKSIZE;
-            break;
-        case 'D':
-            snakePosY[0] = snakePosY[0] + TICKSIZE;
-            break;
-        case 'L':
-            snakePosX[0] = snakePosX[0] - TICKSIZE;
-            break;
-        case 'R':
-            snakePosX[0] = snakePosX[0] + TICKSIZE;
-            break;
-    }
-}
-
-    
-
     protected void start() {
         snakePosX = new int[BOARDSIZE];
         snakePosY = new int[BOARDSIZE];
@@ -108,9 +74,75 @@ protected void move() {
     }
 
     @Override
+    protected void paintComponent(java.awt.Graphics g) {
+        super.paintComponent(g);
+        if (isMoving) {
+            g.setColor(Color.darkGray);
+            for (int i = 0; i < snakeLength; i++) {
+                g.setColor(Color.BLUE);
+                g.fillRect(snakePosX[i], snakePosY[i], TICKSIZE, TICKSIZE);
+
+            }
+        } else {
+            String gameOver = String.format("Game Over \n Score: %d, Press any key to restart.", snakeLength);
+            g.setColor(Color.red);
+            g.setFont(font);
+            g.drawString(gameOver, (WIDTH - getFontMetrics(g.getFont()).stringWidth(gameOver)) / 2, HEIGHT / 2);
+        }
+    }
+
+    protected void move() {
+        for (int i = snakeLength; i > 0; i--) {
+            snakePosX[i] = snakePosX[i - 1];
+            snakePosY[i] = snakePosY[i - 1];
+        }
+        switch (direction) {
+            case 'U':
+                snakePosY[0] = snakePosY[0] - TICKSIZE;
+                break;
+            case 'D':
+                snakePosY[0] = snakePosY[0] + TICKSIZE;
+                break;
+            case 'L':
+                snakePosX[0] = snakePosX[0] - TICKSIZE;
+                break;
+            case 'R':
+                snakePosX[0] = snakePosX[0] + TICKSIZE;
+                break;
+        }
+    }
+    protected void collision() {
+        for (int i = snakeLength; i > 0; i--) {
+            if ((snakePosX[0] == snakePosX[i]) && (snakePosY[0] == snakePosY[i])) {
+                isMoving = false;
+                break;
+            }
+        }
+        if (snakePosX[0] < 0) {
+            isMoving = false;
+        }
+        if (snakePosX[0] > WIDTH - TICKSIZE) {
+            isMoving = false;
+        }
+        if (snakePosY[0] < 0) {
+            isMoving = false;
+        }
+        if (snakePosY[0] > HEIGHT - TICKSIZE) {
+            isMoving = false;
+        }
+        if (!isMoving) {
+            timer.stop();
+        }
+    }
+    
+
+    
+
+    @Override
     public void actionPerformed(ActionEvent e) {
         if (isMoving) {
             move();
+            collision();
         }
         repaint();
     }
